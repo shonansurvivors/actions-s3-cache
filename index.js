f0012288de3b0fafc267d624c61ca3e00f4e16b6
changwei <changwei@getjerry.com>
2021-12-15 12:25:55 +0800

    fix: throw exception if exec has stderr output

--- index.js
+++ index.js
@@ -25,8 +25,20 @@ async function run() {
       }, async (err, data) => {
         if (err) {
           console.log(`No cache is found for key: ${fileName}`);
+          let commandError = '';
 
-          await exec.exec(command); // install or build command e.g. npm ci, npm run dev
+          // install or build command e.g. npm ci, npm run dev
+          await exec.exec(command,[],{
+            listeners: {
+              stderr: (data) => {
+                commandError += data.toString().trim();
+              }
+            }
+          });
+          if (commandError) {
+            throw new Error(commandError);
+          }
+
           await exec.exec(`zip ${zipOption} ${fileName} ${paths}`);
 
           s3.upload({
